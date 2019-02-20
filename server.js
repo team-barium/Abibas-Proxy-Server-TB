0d6ec0849290f475fa97d5b1b043a812a4f15b98
Taylor Bantle <bantle.taylor@gmail.com>
2019-02-20 11:52:41 -0800

    Set up server and static files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
